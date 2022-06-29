41bac4feb815fd163236e907b608e589fb89c492
deigo-lps <diego.lopes0909@gmail.com>
2022-06-28 21:13:48 -0300

    path variables

--- rest.api/src/main/java/com/rest/api/rest/api/HelloWorldController.java
+++ rest.api/src/main/java/com/rest/api/rest/api/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.api.rest.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World",2);
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}/{value}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name,@PathVariable Integer value) {
+        return new HelloWorldBean(String.format("Hello World, %s",name),value);
     }
 
 }
